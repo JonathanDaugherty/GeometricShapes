47b58e6140c978a3d391e46ab9efe5232884f207
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-02-15 11:54:00 -0500

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GeometricShapes {
+    class Program {
+        static void Main(string[] args) {
+
+            var sqr1 = new Square(5);
+            Console.WriteLine($"sqr1 perimeter is {sqr1.Perimeter()}, sqr1 area is {sqr1.area()}");
+            
+            
+            /* var rect1 = new Rect(6, 4);
+            var perimeter = rect1.Perimeter();
+            var area = rect1.Area();
+            Console.WriteLine($"Rect1's perimeter is {perimeter}, Rect1's area is {area}"); 
+
+            var rect1 = new Rect(6, 4);
+            Console.WriteLine($"rect1 perimeter is {rect1.Perimeter()}, rect1 area is {rect1.Area()}");
+
+
+            var Rect2 = new Rect(5, 8);
+            Console.WriteLine($"Rect2 perimeter is {Rect2.Perimeter()}, Rect2 area is {Rect2.Area()}"); */
+            
+           /* var quad1 = new Quad(3, 4, 5, 6);
+            var perimeter = quad1.Perimeter();
+            Console.WriteLine($"quad1's perimeter is {perimeter}");
+
+            var quad2 = new Quad();
+            Console.WriteLine($"quad 2's perimeter is {quad2.Perimeter()}"); */
+        }
+    }
+}
---
+++ GeometricShapes/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricShapes {
+    class Square {
+        public int Side { get; set; }
+        public int Area { get; set; }
+        
+        public int Perimeter() {
+            return Side * 4;
+        }
+
+        public int area() {
+            return Side * Side;
+
+        }
+
+        public Square() {
+
+        }
+
+        public Square(int s1) {
+            Side = s1;
+        }
+    }
+
+}
